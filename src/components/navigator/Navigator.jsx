cd328b96b1023527ac5affe11e9d7f7efe5bf090
m.samiyan <ms_MS123456>
2023-05-09 17:50:38 +0430

    rename

--- src/components/navigator/Navigator.jsx
+++ src/components/navigator/Navigator.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { ReactComponent as Agenda } from '../../assets/moshkl.svg';
+import { ReactComponent as Agenda } from '../../assets/Agenda.svg';
 import { ReactComponent as Tasks } from '../../assets/Tasks.svg';
 import { ReactComponent as MainButton } from '../../assets/MainButton.svg';
 import { ReactComponent as Notification } from '../../assets/Notification.svg';
